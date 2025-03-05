579d284342828618387eac85f6326c44dde3d462
Jose Maza <josedmaza21@gmail.com>
2025-03-05 17:08:11 -0500

    -Modificaciones para el despliegue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // URL del backend
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api', // Fallback para local
 });
 
 export default api;
